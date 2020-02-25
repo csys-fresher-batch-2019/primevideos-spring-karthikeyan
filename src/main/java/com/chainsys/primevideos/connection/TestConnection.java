03b90c4dd1f6c22b3e32b6105aea1e04db7c24bc
Karthikeyan P <bmpdk123@gmail.com>
2020-02-25 13:02:34 +0530

    25/2 13.02

--- src/main/java/com/chainsys/primevideos/connection/TestConnection.java
+++ src/main/java/com/chainsys/primevideos/connection/TestConnection.java
@@ -1,12 +1,14 @@
 package com.chainsys.primevideos.connection;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.TimeZone;
 public class TestConnection {
 	private TestConnection() {
 	}
 	public static Connection getConnection()  throws Exception{
 		Class.forName("oracle.jdbc.driver.OracleDriver");
-		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","oracle");
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@13.235.147.120:1521:XE","karthikeyan","karthikeyan");
 		return connection;
 	}
 }
